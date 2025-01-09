a6a29191d58e15bbadaa3d65b72b0da5e1683039
ETUKURI SURENDRA BABU <112386357+esurendrababu@users.noreply.github.com>
2025-01-09 13:21:50 +0530

    Update app.py

--- project/app.py
+++ project/app.py
@@ -25,4 +25,5 @@ def download_resume():
     return send_from_directory(directory, filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
