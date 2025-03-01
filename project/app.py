f346645880bc690e1d7be5f0315e1c4a4ce2af9f
ETUKURI SURENDRA BABU <112386357+esurendrababu@users.noreply.github.com>
2025-03-01 14:25:42 +0530

    Update app.py

--- project/app.py
+++ project/app.py
@@ -25,5 +25,6 @@ def download_resume():
     return send_from_directory(directory, filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    pass  # Gunicorn will serve the app
+
 
